0223067ce8c7470b2651c5d80a5947017b3ebba2
laizuakram1 <laizuakram98@gmail.com>
2022-05-20 13:02:59 +0600

    create data data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
+
+//middle ware
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -17,11 +22,42 @@ async function run(){
     const equipmentCollection = client.db("equipments").collection("equipment");
     
     //get products to mongodb
+
+    // http://localhost:5000/products
     app.get('/products', async (req, res)=>{
       const query = req.body;
       const cursor = equipmentCollection.find(query);
       const result = await cursor.toArray();
 
+      res.send(result);
+    })
+
+    //post data to mongodb
+    app.post('/porduct', async(req, res) =>{
+      const data = req.body;
+      // const data = {name: "akram", text: "Hello"};
+      const result = await equipmentCollection.insertOne(data);
+      console.log( `inseted id:${result.insertedId}`);
+      
+      res.send(result);
+    })
+
+    // udate data to mongodb
+
+    // http://localhost:5000/product/6287386ad467649e6b2cee18
+    app.put('/product/:id',async(req,res) =>{
+      const data = req.body;
+      const id = req.params.id;
+      const filter = {_id:ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...data
+        },
+      };
+      const result = await equipmentCollection.updateOne(filter, updateDoc, options);
+      console.log('udate data');
+      
       res.send(result);
     })
 
